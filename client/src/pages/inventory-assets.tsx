4152066eeb8dde510661b8836c1a1e5c34edf4ef
Nelson Perez <157074864+nperezv@users.noreply.github.com>
2026-02-26 09:38:43 +0100

    Revert "feat(inventory): iOS-style Inventory UI with scan, move and audit flows"

--- client/src/pages/inventory-assets.tsx
+++
@@ -1,5 +0,0 @@
-import { InventoryAssetsPage } from "@/modules/inventory/pages/assets-page";
-
-export default function InventoryAssetsRoute() {
-  return <InventoryAssetsPage />;
-}
--- client/src/pages/inventory-scan.tsx
+++
@@ -1,5 +0,0 @@
-import { InventoryScanPage } from "@/modules/inventory/pages/scan-page";
-
-export default function InventoryScanRoute() {
-  return <InventoryScanPage />;
-}
